ac7874d5909aaf0d620248ddd847eed828750307
lacerdam3423-pixel <lacerdam3423@gmail.com>
2026-04-12 12:48:51 -0400

    Update BrightModeFNAFSOM.lua

--- BrightModeFNAFSOM.lua
+++ BrightModeFNAFSOM.lua
@@ -39,7 +39,7 @@ local function transformObject(v)
         
         -- Tudo que era Transparency 1 vira 0.75
         if v.Transparency == 1 then
-            v.Transparency = 0.45
+            v.Transparency = 0.75
         end
 
     -- Lógica de Decals (Transparent 1 vira 0 e mantém os outros visíveis)
